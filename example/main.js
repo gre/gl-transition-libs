d3f746fe6a306eb23e50b8fc57b71ae7057fed98
Gaëtan Renaudeau <gre@zenexity.com>
2014-01-05 10:09:57 +0100

    add 2 presets for squares

--- example/main.js
+++ example/main.js
@@ -10,7 +10,8 @@ var transitions;
 var canvas = document.getElementById("viewport");
 var Transition = GlslTransition(canvas);
 transitions = {
-  "squares"    : Transition(require("./transitions/squares.glsl"), { size: [64, 45], smoothness: 0.2 }),
+  "squares1"   : Transition(require("./transitions/squares.glsl"), { size: [13, 9], smoothness: 0.5 }),
+  "squares2"   : Transition(require("./transitions/squares.glsl"), { size: [64, 45], smoothness: 0.2 }),
   "ripple"     : Transition(require("./transitions/ripple.glsl"), { amplitude: 100, speed: 50 }),
   "flyeye1"    : Transition(require("./transitions/flyeye.glsl"), { size: 0.04, zoom: 20.0 }),
   "flyeye2"    : Transition(require("./transitions/flyeye.glsl"), { size: 0.1, zoom: 40.0 }),
