7fbcaeba425f1038694b1a6cb45c9e40ac4eac75
Gaëtan Renaudeau <renaudeau.gaetan@gmail.com>
2017-07-24 13:10:15 +0200

    update

--- packages/gl-transition-bot/bin/generate-review.sh
+++ packages/gl-transition-bot/bin/generate-review.sh
@@ -2,6 +2,7 @@
 
 #env vars: GITHUB_TOKEN IMGUR_KEY PULL_REQUEST
 
+SCRPTDIR=`dirname $0`
 set -e
 cd `mktemp -d`
 git clone https://github.com/gl-transitions/gl-transitions.git
@@ -9,4 +10,4 @@ cd gl-transitions
 git fetch origin pull/$PULL_REQUEST/head:pr
 git checkout pr
 # Xvfb :99 & export DISPLAY=:99
-xvfb-run -s "-ac -screen 0 1280x1024x24" gl-transition-bot-review-pr
+xvfb-run -s "-ac -screen 0 1280x1024x24" node $SCRPTDIR/generate-review.js
